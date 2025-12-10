dfabfdcdb57ff9f61808a62b513904e9c66fbb5b
HARDY SEBASTIAN GANOZA IPANAQUE <U21206623@utp.edu.pe>
2025-12-10 07:16:04 -0500

    Actualizado

---
+++ VUE-APP-INTERBANK/Pages/Transferencia.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VUE_APP_INTERBANK.Pages.Shared
+{
+    public class TransferenciaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
